ff6ddab33cbc2e95092588e94de4c554d0d84245
luanspl@hotmail.com <luanspl@hotmail.com@5b0c7643-8a35-019b-faec-b44fde28125f>
2012-06-16 12:21:16 +0000

    Programa em QT, utilizando OpenGL, fazendo rodar uma pirâmide

---
+++ Piramide_opengl/main.cpp
@@ -0,0 +1,13 @@
+#include <QtGui/QApplication>
+#include "principal.h"
+
+
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    principal w;
+    w.show();
+    
+    return a.exec();
+}
